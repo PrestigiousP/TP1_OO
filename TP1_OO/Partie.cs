1329b2dc6030fcc9490709935dee78eade6baba2
PrestigiousP <65975189+PrestigiousP@users.noreply.github.com>
2020-09-18 14:04:06 -0400

    J'ai ajouté les cartes

--- TP1_OO/Partie.cs
+++ TP1_OO/Partie.cs
@@ -6,8 +6,62 @@ namespace TP1_OO
 {
     class Partie
     {
+        //Cartes de coeur.
         Carte asCoeur = new Carte(1, Carte.Couleur.Coeur);
-        //Carte 
+        Carte deuxCoeur = new Carte(2, Carte.Couleur.Coeur);
+        Carte troisCoeur = new Carte(3, Carte.Couleur.Coeur);
+        Carte quatreCoeur = new Carte(4, Carte.Couleur.Coeur);
+        Carte cinqCoeur = new Carte(5, Carte.Couleur.Coeur);
+        Carte sixCoeur = new Carte(6, Carte.Couleur.Coeur);
+        Carte septCoeur = new Carte(7, Carte.Couleur.Coeur);
+        Carte huitCoeur = new Carte(8, Carte.Couleur.Coeur);
+        Carte neufCoeur = new Carte(9, Carte.Couleur.Coeur);
+        Carte dixCoeur = new Carte(10, Carte.Couleur.Coeur);
+        Carte valetCoeur = new Carte(11, Carte.Couleur.Coeur);
+        Carte dameCoeur = new Carte(12, Carte.Couleur.Coeur);
+        Carte roiCoeur = new Carte(13, Carte.Couleur.Coeur);
+        //Cartes de carreau.
+        Carte asCarreau = new Carte(1, Carte.Couleur.Carreau);
+        Carte deuxCarreau = new Carte(2, Carte.Couleur.Carreau);
+        Carte troisCarreau = new Carte(3, Carte.Couleur.Carreau);
+        Carte quatreCarreau = new Carte(4, Carte.Couleur.Carreau);
+        Carte cinqCarreau = new Carte(5, Carte.Couleur.Carreau);
+        Carte sixCarreau = new Carte(6, Carte.Couleur.Carreau);
+        Carte septCarreau = new Carte(7, Carte.Couleur.Carreau);
+        Carte huitCarreau = new Carte(8, Carte.Couleur.Carreau);
+        Carte neufCarreau = new Carte(9, Carte.Couleur.Carreau);
+        Carte dixCarreau = new Carte(10, Carte.Couleur.Carreau);
+        Carte valetCarreau = new Carte(11, Carte.Couleur.Carreau);
+        Carte dameCarreau = new Carte(12, Carte.Couleur.Carreau);
+        Carte roiCarreau = new Carte(13, Carte.Couleur.Carreau);
+        //Cartes de trefle.
+        Carte asTrefle = new Carte(1, Carte.Couleur.Trefle);
+        Carte deuxTrefle = new Carte(2, Carte.Couleur.Trefle);
+        Carte troisTrefle = new Carte(3, Carte.Couleur.Trefle);
+        Carte quatreTrefle = new Carte(4, Carte.Couleur.Trefle);
+        Carte cinqTrefle = new Carte(5, Carte.Couleur.Trefle);
+        Carte sixTrefle = new Carte(6, Carte.Couleur.Trefle);
+        Carte septTrefle = new Carte(7, Carte.Couleur.Trefle);
+        Carte huitTrefle = new Carte(8, Carte.Couleur.Trefle);
+        Carte neufTrefle = new Carte(9, Carte.Couleur.Trefle);
+        Carte dixTrefle = new Carte(10, Carte.Couleur.Trefle);
+        Carte valetTrefle = new Carte(11, Carte.Couleur.Trefle);
+        Carte dameTrefle = new Carte(12, Carte.Couleur.Trefle);
+        Carte roiTrefle = new Carte(13, Carte.Couleur.Trefle);
+        //Cartes de pique.
+        Carte asPique = new Carte(1, Carte.Couleur.Pique);
+        Carte deuxPique = new Carte(2, Carte.Couleur.Pique);
+        Carte troisPique = new Carte(3, Carte.Couleur.Pique);
+        Carte quatrePique = new Carte(4, Carte.Couleur.Pique);
+        Carte cinqPique = new Carte(5, Carte.Couleur.Pique);
+        Carte sixPique = new Carte(6, Carte.Couleur.Pique);
+        Carte septPique = new Carte(7, Carte.Couleur.Pique);
+        Carte huitPique = new Carte(8, Carte.Couleur.Pique);
+        Carte neufPique = new Carte(9, Carte.Couleur.Pique);
+        Carte dixPique = new Carte(10, Carte.Couleur.Pique);
+        Carte valetPique = new Carte(11, Carte.Couleur.Pique);
+        Carte damePique = new Carte(12, Carte.Couleur.Pique);
+        Carte roiPique = new Carte(13, Carte.Couleur.Pique);
 
     }
 }
